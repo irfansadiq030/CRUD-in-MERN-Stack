8c638dce8c04b3f0003b5b70f02202c63866848c
irfansadiqdevzone <irfansadiq030@gmail.com>
2022-06-05 19:24:08 +0500

    Alert Added
    --When clicked on Delete button there will be display a popup to confirm the deletion of the user.

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import './cards.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUsers } from '../action/UserAction'
@@ -19,15 +19,39 @@ export const UserList = () => {
 
     }, [dispatch])
 
-    const deleteUser = async (id,e) => {
+    // Delete User Function
+    
+    const [userId, setUserId] = useState('');
+    const closeRef = useRef(null);
+
+    const deleteUser = async (id, e) => {
         e.preventDefault();
-        // alert(id)
+        
         await axios.delete(`http://localhost:3500/api/v1/user/${id}`);
 
+        closeRef.current.click();
         dispatch(getUsers())
+
     }
     return (
-        < >
+        <>
+            {/* Delete POPUP/MODAL */}
+            <div class="modal fade" id="deleteUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Delete User</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <h5 className='mb-4'>Are you sure, you want to Delete?</h5>
+                            <a onClick={(e) => deleteUser(userId,e)} href="/" className="btn btn-danger">Delete</a>
+                            <a ref={closeRef} href="/" className="ms-5 btn btn-secondary"  data-bs-dismiss="modal" aria-label="Close">Cancel</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
             {
                 (loading) ?
                     <div className="container loading_container">
@@ -53,16 +77,16 @@ export const UserList = () => {
                                         <h5 className="card-title">{user.name}</h5>
                                         <h6>Email : {user.email}</h6>
                                         <a href="/" className="btn btn-primary mt-2">Edit User</a>
-                                        <a onClick={(e)=>deleteUser(user._id,e)} href="/" className="ms-3 btn btn-danger mt-2">Delete User</a>
+                                        <a onClick={() => setUserId(user._id)} href="/" className="ms-3 btn btn-danger mt-2" data-bs-toggle="modal" data-bs-target="#deleteUser">Delete User</a>
+                                    </div>
                                 </div>
                             </div>
-                            </div>
-                ))
+                        ))
                     }
 
 
+                </div>
             </div>
-        </div>
         </>
     )
 }
